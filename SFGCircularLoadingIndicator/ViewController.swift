efb3b9780ec38d19e4ac97e23ade6b34cebf2b42
Jon-Tait Beason <jazbo1876@gmail.com>
2020-03-21 08:30:49 -0700

    Initial Commit

---
+++ SFGCircularLoadingIndicator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SFGCircularLoadingIndicator
+//
+//  Created by Jon-Tait Beason on 3/21/20.
+//  Copyright © 2020 Jon-Tait Beason. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
